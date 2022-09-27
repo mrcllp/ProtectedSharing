2750e942ffdcb2d48145eb7e1d2d2e8cf140b4fd
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-27 18:53:47 +0200

    mongo db/url

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+require("dotenv").config()
 const multer = require("multer")
+const mongoose = require("mongoose")
 
 const express = require("express")
 const req = require("express/lib/request")
 const app = express()
 
 const upload = multer({ dest: "uploads"})
+
+mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
 
@@ -17,4 +21,4 @@ app.post("/upload", upload.single("file"), (req, res) => {
     res.send('hi')
 })
 
-app.listen(5000)
+app.listen(process.env.PORT)

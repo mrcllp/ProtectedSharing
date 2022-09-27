8b3093f9df6e2aeb3430ca8fff268504019f5376
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-27 18:49:20 +0200

    multer

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+const multer = require("multer")
+
 const express = require("express")
+const req = require("express/lib/request")
 const app = express()
+
+const upload = multer({ dest: "uploads"})
 
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
     res.render("index")
+})
+
+// listen to post request from form
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.send('hi')
 })
 
 app.listen(5000)
